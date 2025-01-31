c2b078d2a3a4796f41a708fd2afc526f5dc7db20
chiragvips <106860835+chiragvips@users.noreply.github.com>
2025-01-31 12:34:50 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -12,7 +12,11 @@ function greet(){
     return meet;
 }
 
-
+// with the help of closure the iternal function can hold outside varibale access in it 
+// we cant print a oustide the function as it scope only exist within the local scope of the function
+// with the help of closure we can acess variable even if there scope doesnt exist 
+// as we can access a and b after get removed greet() function memory from stack as functions are stored in heap memory  
 const num = greet();
 num();
+
 
